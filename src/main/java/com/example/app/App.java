bbfad5f98722c8c2558f594af2d5bd17d20313d0
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2024-05-24 19:26:47 +0200

    parte5 END

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -2,6 +2,7 @@ package com.example.app;
 
 import java.util.Scanner;
 
+import com.example.cart.ShoppingRegister;
 import com.example.sales.SalesRegister;
 import com.example.shop.ClothingItem;
 import com.example.shop.Inventory;
@@ -15,6 +16,7 @@ import com.example.test.TestLinkedCart;
 public class App {
     private static Scanner tec = new Scanner(System.in);
     private static Inventory inventory = new Inventory(20);
+    private static ShoppingRegister shoppingRegister = new ShoppingRegister();
 
     public static void printMenu() {
         System.out.println("*** Bienvenido a Strafalarius ***");
@@ -23,8 +25,12 @@ public class App {
         System.out.println("2. Mostrar inventario");
         System.out.println("3. Procesar venta");
         System.out.println("4. Mostrar estadisticas de venta");
-        System.out.println("5. Salir");
-        System.out.println("Seleccione una opción (1-5): ");
+        System.out.println("5. Agregar prenda al carrito");
+        System.out.println("6. Eliminar prenda al carrito");
+        System.out.println("7. Confirmar carrito de compra");
+        System.out.println("8. Mostrar Productos del carrito");
+        System.out.println("9. Salir");
+        System.out.println("Seleccione una opción (1-9): ");
     }
 
     public static int userSelect() {
@@ -69,10 +75,38 @@ public class App {
         System.out.println("Ventas count: " + SalesRegister.getTotalSalesCount());
     }
 
+    public static void option5() {
+        System.out.println("Name:");
+        String name = tec.nextLine();
+        System.out.println("Size:");
+        char size = tec.nextLine().charAt(0);
+        System.out.println("Cantidad:");
+        int cantidad = Integer.parseInt(tec.nextLine());
+        shoppingRegister.addToCart(inventory, name, size, cantidad);
+    }
+
+    public static void option6() {
+        System.out.println("Name:");
+        String name = tec.nextLine();
+        System.out.println("Size:");
+        char size = tec.nextLine().charAt(0);
+        System.out.println("Cantidad:");
+        int cantidad = Integer.parseInt(tec.nextLine());
+        shoppingRegister.removeFromCart(inventory, name, size, cantidad);
+    }
+
+    public static void option7() {
+        shoppingRegister.confirmCart(inventory);
+    }
+
+    public static void option8() {
+        shoppingRegister.showCart();
+    }
+
     public static void menu() {
         int option = 0;
 
-        while (option != 5) {
+        while (option != 9) {
             printMenu();
             option = userSelect();
             switch (option) {
@@ -89,6 +123,18 @@ public class App {
                     option4();
                     break;
                 case 5:
+                    option5();
+                    break;
+                case 6:
+                    option6();
+                    break;
+                case 7:
+                    option7();
+                    break;
+                case 8:
+                    option8();
+                    break;
+                case 9:
                     break;
                 default:
                     System.out.println("La opción debe de ser un número entre 1 y 5.");
@@ -103,10 +149,10 @@ public class App {
         // TestClothingItem.checkClass(ClothingItem.class);
         // TestInventory.checkClass(Inventory.class);
         // TestSalesRegister.checkSalesRegister();
-        TestLinkedCart.checkLinkedCart();
-        TestShoppingRegister.checkShoppingRegister();
+        // TestLinkedCart.checkLinkedCart();
+        // TestShoppingRegister.checkShoppingRegister();
 
-        // menu();
+        menu();
 
     }
 }
